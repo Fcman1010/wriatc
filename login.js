3fae8fd91dcfdcf83f7fec87376fad3cbb17818f
Fcman1010 <fcman1690@gmail.com>
2024-08-29 22:41:36 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 document.getElementById('login-form').addEventListener('submit', function(e) {
     e.preventDefault();
     const name = document.getElementById('name').value;
-    const validName = 'ใส่ชื่อ'; // ใส่ชื่อเจ้าของวันเกิดที่ต้องการตรงนี้
+    const validName = 'เค้ก'; // ใส่ชื่อเจ้าของวันเกิดที่ต้องการตรงนี้
 
     if (name === validName) {
         window.location.href = 'birthday.html';
